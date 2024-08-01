9a1e1adf84c29e4f244586d5ffef420f9c5ce24e
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2024-08-01 19:18:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,12 +17,12 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+API_ID       = int(environ.get("API_ID", "19094598"))
+API_HASH     = environ.get("API_HASH", "479c00e863915ba2f8b90d8030ca4786")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7329951541:AAHWqJNmIiq0GTpA3RNv56SIzPUU9qXh_5k")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://anmol:anmol@cluster0.eaq9icz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002114115777"))
+ADMIN        = int(environ.get("ADMIN", "1663603208"))
 CHANNEL      = environ.get("CHANNEL", "@Hara_Tg")
 PICS         = environ.get('PICS', 'https://telegra.ph/file/288c886149072f44e080f.jpg https://telegra.ph/file/4ece835d581d2c4fef29e.jpg https://telegra.ph/file/b363ccae2b057dc029c5e.jpg https://telegra.ph/file/d38f165aaa36dc212d372.jpg').split()
 UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', '-1002128004223')
